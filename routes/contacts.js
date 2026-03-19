197473e62bd8216b7078abe1286bc47f71a39684
Guilherme <pbatista.guilherme@gmail.com>
2026-03-19 14:41:16 -0300

    week03

--- routes/contacts.js
+++ routes/contacts.js
@@ -2,11 +2,127 @@ const express = require('express');
 const router = express.Router();
 const contactsController = require('../controllers/contacts');
 
+
+
+/**
+ * @swagger
+ * /contacts:
+ *   get:
+ *     summary: Get all contacts
+ *     tags: [Contacts]
+ *     responses:
+ *       200:
+ *         description: List of contacts
+ */
 router.get('/', contactsController.getAllContacts);
+
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   get:
+ *     summary: Get a single contact by ID
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Contact found
+ *       404:
+ *         description: Contact not found
+ */
 router.get('/:id', contactsController.getSingleContact);
+  /**
+ * @swagger
+ * /contacts:
+ *   post:
+ *     summary: Create a new contact
+ *     tags: [Contacts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - firstName
+ *               - lastName
+ *               - email
+ *               - favoriteColor
+ *               - birthday
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Contact created
+ */
 
 router.post('/', contactsController.createContact);
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   put:
+ *     summary: Update a contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstName:
+ *                 type: string
+ *               lastName:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               favoriteColor:
+ *                 type: string
+ *               birthday:
+ *                 type: string
+ *     responses:
+ *       204:
+ *         description: Contact updated successfully
+ */
 router.put('/:id', contactsController.updateContact);
+/**
+ * @swagger
+ * /contacts/{id}:
+ *   delete:
+ *     summary: Delete a contact
+ *     tags: [Contacts]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Contact ID
+ *         schema:
+ *           type: string
+ *     responses:
+ *       204:
+ *         description: Contact deleted successfully
+ */
 router.delete('/:id', contactsController.deleteContact);
 
 
--- server.js
+++ server.js
@@ -5,9 +5,27 @@ const mongodb = require('./data/database');
 const express = require('express');
 const app = express();
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Contacts API",
+      version: "1.0.0"
+    }
+  },
+  apis: ["./routes/*.js"]
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
 const PORT = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 mongodb.initDb((err) => {
   if (err) {
